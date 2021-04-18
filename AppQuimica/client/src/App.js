a6dad3808b4a2736165cfebf5c934b757e8c3bd7
Sherab Pereira <sherabpereira@gmail.com>
2021-04-18 21:07:45 +0200

    Cambio en la estructura, refactoring, creación del proyecto front con React, pruebas con Chart.js y jspdf

---
+++ AppQuimica/client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ScatterChart from './components/ScatterChart'
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div>
+      <ScatterChart></ScatterChart>
+    </div>
+  )
+}
+
+export default App
