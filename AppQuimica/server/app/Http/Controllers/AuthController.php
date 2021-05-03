2dde7f791b4e726cd15ce17e8198a5ddb81830f7
Toni <tonisoria2000@gmail.com>
2021-05-03 19:53:50 +0200

    authcontroller changes from master

--- AppQuimica/server/app/Http/Controllers/AuthController.php
+++ AppQuimica/server/app/Http/Controllers/AuthController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Alumno;
-use App\Models\Profesor;
-use App\Models\Usuario;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
@@ -18,8 +15,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login', 'registro', 'registroAlumno', 'registroProfesor',
-            'eliminarUsuario', 'leerUsuario', 'updateUsuario']]);
+        $this->middleware('auth:api', ['except' => ['login']]);
     }
 
     /**
@@ -45,35 +41,35 @@ class AuthController extends Controller
         return $this->createNewToken($token);
     }
 
-    /**
-     * Register a User.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function registro(Request $request)
-    {
+    // /**
+    //  * Register a User.
+    //  *
+    //  * @return \Illuminate\Http\JsonResponse
+    //  */
+    // public function registro(Request $request)
+    // {
 
-        $validator = Validator::make($request->all(), [
-            'username' => 'required|string|between:2,100',
-            'password' => 'required|string|confirmed|min:6',
-            'id_profesor' => 'integer',
-            'id_alumno' => 'integer',
-        ]);
+    //     $validator = Validator::make($request->all(), [
+    //         'username' => 'required|string|between:2,100',
+    //         'password' => 'required|string|confirmed|min:6',
+    //         'id_profesor' => 'integer',
+    //         'id_alumno' => 'integer',
+    //     ]);
 
-        if ($validator->fails()) {
-            return response()->json($validator->errors()->toJson(), 400);
-        }
+    //     if ($validator->fails()) {
+    //         return response()->json($validator->errors()->toJson(), 400);
+    //     }
 
-        $user = Usuario::create(array_merge(
-            $validator->validated(),
-            ['password' => bcrypt($request->password)]
-        ));
+    //     $user = Usuario::create(array_merge(
+    //         $validator->validated(),
+    //         ['password' => bcrypt($request->password)]
+    //     ));
 
-        return response()->json([
-            'message' => 'Registrado con éxito',
-            'user' => $user,
-        ], 201);
-    }
+    //     return response()->json([
+    //         'message' => 'Registrado con éxito',
+    //         'user' => $user
+    //     ], 201);
+    // }
 
     /**
      * Log the user out (Invalidate the token).
@@ -129,156 +125,156 @@ class AuthController extends Controller
         ]);
     }
 
-    /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function registroAlumno(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'username' => 'required|string|between:2,100',
-            'password' => 'required|string|confirmed|min:6',
-            'id_profesor' => 'integer',
-            'id_alumno' => 'integer',
-        ]);
+    // /**
+    //  * Get a JWT via given credentials.
+    //  *
+    //  * @return \Illuminate\Http\JsonResponse
+    //  */
+    // public function registroAlumno(Request $request)
+    // {
+    //     $validator = Validator::make($request->all(), [
+    //         'username' => 'required|string|between:2,100',
+    //         'password' => 'required|string|confirmed|min:6',
+    //         'id_profesor' => 'integer',
+    //         'id_alumno' => 'integer',
+    //     ]);
 
-        if ($validator->fails()) {
-            return response()->json($validator->errors()->toJson(), 400);
-        }
+    //     if ($validator->fails()) {
+    //         return response()->json($validator->errors()->toJson(), 400);
+    //     }
 
-        $alumno = new Alumno;
+    //     $alumno = new Alumno;
 
-        $alumno->id_grupo = $request->id_grupo;
-        $alumno->nombre = $request->nombre;
-        $alumno->apellidos = $request->apellidos;
-        $alumno->email = $request->email;
+    //     $alumno->id_grupo = $request->id_grupo;
+    //     $alumno->nombre = $request->nombre;
+    //     $alumno->apellidos = $request->apellidos;
+    //     $alumno->email = $request->email;
 
-        $alumno->save();
+    //     $alumno->save();
 
-        $user = Usuario::create(array_merge(
-            $validator->validated(),
-            ['password' => bcrypt($request->password)],
-            ['id_alumno' => $alumno->id]
-        ));
+    //     $user = Usuario::create(array_merge(
+    //         $validator->validated(),
+    //         ['password' => bcrypt($request->password)],
+    //         ['id_alumno' => $alumno->id]
+    //     ));
 
-        return response()->json([
-            'message' => 'Registrado con éxito',
-            'user' => $user,
-        ], 201);
-    }
+    //     return response()->json([
+    //         'message' => 'Registrado con éxito',
+    //         'user' => $user
+    //     ], 201);
+    // }
 
-    /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function registroProfesor(Request $request)
-    {
+    // /**
+    //  * Get a JWT via given credentials.
+    //  *
+    //  * @return \Illuminate\Http\JsonResponse
+    //  */
+    // public function registroProfesor(Request $request)
+    // {
 
-        $validator = Validator::make($request->all(), [
-            'username' => 'required|string|between:2,100',
-            'password' => 'required|string|confirmed|min:6',
-            'id_profesor' => 'integer',
-            'id_alumno' => 'integer',
-        ]);
+    //     $validator = Validator::make($request->all(), [
+    //         'username' => 'required|string|between:2,100',
+    //         'password' => 'required|string|confirmed|min:6',
+    //         'id_profesor' => 'integer',
+    //         'id_alumno' => 'integer',
+    //     ]);
 
-        if ($validator->fails()) {
-            return response()->json($validator->errors()->toJson(), 400);
-        }
+    //     if ($validator->fails()) {
+    //         return response()->json($validator->errors()->toJson(), 400);
+    //     }
 
-        $profesor = new Profesor;
+    //     $profesor = new Profesor;
 
-        $profesor->nombre = $request->nombre;
-        $profesor->apellidos = $request->apellidos;
-        $profesor->email = $request->email;
-        $profesor->es_admin = $request->es_admin;
+    //     $profesor->nombre = $request->nombre;
+    //     $profesor->apellidos = $request->apellidos;
+    //     $profesor->email = $request->email;
+    //     $profesor->es_admin = $request->es_admin;
 
-        $profesor->save();
+    //     $profesor->save();
 
-        $user = Usuario::create(array_merge(
-            $validator->validated(),
-            ['password' => bcrypt($request->password)],
-            ['id_profesor' => $profesor->id]
-        ));
+    //     $user = Usuario::create(array_merge(
+    //         $validator->validated(),
+    //         ['password' => bcrypt($request->password)],
+    //         ['id_profesor' => $profesor->id]
+    //     ));
 
-        return response()->json([
-            'message' => 'Registrado con éxito',
-            'user' => $user,
-        ], 201);
-    }
+    //     return response()->json([
+    //         'message' => 'Registrado con éxito',
+    //         'user' => $user
+    //     ], 201);
+    // }
 
-    public function eliminarUsuario($id)
-    {
-        $usuario = Usuario::find($id);
+    // public function eliminarUsuario($id)
+    // {
+    //     $usuario = Usuario::find($id);
 
-        if ($usuario->id_profesor) {
-            $profesor = Profesor::find($usuario->id_profesor);
-            $profesor->delete();
-        } else {
-            $alumno = Alumno::find($usuario->id_alumno);
-            $alumno->delete();
-        }
-        $usuario->delete();
+    //     if($usuario->id_profesor){
+    //         $profesor = Profesor::find($usuario->id_profesor);
+    //         $profesor->delete();
+    //     }else{
+    //         $alumno = Alumno::find($usuario->id_alumno);
+    //         $alumno->delete();
+    //     }
+    //     $usuario->delete();
 
-        return $usuario;
-    }
+    //     return $usuario;
+    // }
 
-    public static function leerUsuario($id)
-    {
-        $usuario = Usuario::find($id);
-        unset($usuario['password']);
-        unset($usuario['token']);
+    // public function leerUsuario($id)
+    // {
+    //     $usuario = Usuario::find($id);
+    //     unset($usuario['password']);
+    //     unset($usuario['token']);
 
-        if ($usuario->id_profesor) {
-            $profesor = Profesor::find($usuario->id_profesor);
-            unset($profesor['es_admin']);
-            return [$usuario, $profesor];
-        } else {
-            $alumno = Alumno::find($usuario->id_alumno);
-            return [$usuario, $alumno];
-        }
-    }
+    //     if($usuario->id_profesor){
+    //         $profesor = Profesor::find($usuario->id_profesor);
+    //         unset($profesor['es_admin']);
+    //         return [$usuario, $profesor];
+    //     }else{
+    //         $alumno = Alumno::find($usuario->id_alumno);
+    //         return [$usuario, $alumno];
+    //     }
+    // }
 
-    /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function updateUsuario(Request $request, $id)
-    {
-        $usuario = Usuario::find($request->id);
+    // /**
+    //  * Get a JWT via given credentials.
+    //  *
+    //  * @return \Illuminate\Http\JsonResponse
+    //  */
+    // public function updateUsuario(Request $request, $id)
+    // {
+    //     $usuario = Usuario::find($request->id);
 
-        if ($request->password) {
-            //$password = bcrypt($request->password);
-            //$usuario->update("password"->$password);
-            $request->password = bcrypt($request->password);
-            $usuario->update(array_merge($request->all(),
-                ['password' => bcrypt($request->password)]));
-        } else {
-            $usuario->update($request->all());
-        }
+    //     if($request->password){
+    //         //$password = bcrypt($request->password);
+    //         //$usuario->update("password"->$password);
+    //         $request->password = bcrypt($request->password);
+    //         $usuario->update(array_merge($request->all(),
+    //         ['password' => bcrypt($request->password)]));
+    //     }else{
+    //         $usuario->update($request->all());
+    //     }
 
-        unset($usuario['password']);
-        unset($usuario['token']);
+    //     unset($usuario['password']);
+    //     unset($usuario['token']);
 
-        if ($usuario->id_alumno) {
-            $alumno = Alumno::find($usuario->id_alumno);
-            $alumno->update($request->all());
-            $respuesta = response()->json([
-                'message' => 'Actualizado con éxito',
-                'user' => [$usuario, $alumno],
-            ], 200);
-        } else {
-            $profesor = Profesor::find($usuario->id_profesor);
-            $profesor->update($request->all());
-            unset($profesor['es_admin']);
-            $respuesta = response()->json([
-                'message' => 'Actualizado con éxito',
-                'user' => [$usuario, $profesor],
-            ], 200);
-        }
+    //     if($usuario->id_alumno){
+    //         $alumno = Alumno::find($usuario->id_alumno);
+    //         $alumno->update($request->all());
+    //         $respuesta = response()->json([
+    //             'message' => 'Actualizado con éxito',
+    //             'user' => [$usuario, $alumno]
+    //         ], 200);
+    //     }else{
+    //         $profesor = Profesor::find($usuario->id_profesor);
+    //         $profesor->update($request->all());
+    //         unset($profesor['es_admin']);
+    //         $respuesta = response()->json([
+    //             'message' => 'Actualizado con éxito',
+    //             'user' => [$usuario, $profesor]
+    //         ], 200);
+    //     }
 
-        return $respuesta;
-    }
+    //     return $respuesta;
+    // }
 }
