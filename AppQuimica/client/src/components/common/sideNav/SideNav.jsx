f16112143ef64a9a713f2283a57debc6e87a4ca0
Sherab Pereira <sherabpereira@gmail.com>
2021-05-05 21:08:32 +0200

    Implementada la barra de menú lateral

--- AppQuimica/client/src/components/common/sideNav/SideNav.jsx
+++ AppQuimica/client/src/components/common/sideNav/SideNav.jsx
@@ -1,17 +1,40 @@
 import React, { useState } from "react";
 import { SNav } from "./SideNav.styled";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 import "./SideNav.css";
+import { SideNavData } from "./SideNavData";
 
-const NavBar = () => {
-  // const [getState, setState] = useState();
+const SideNavBar = () => {
+  const [sidebar, setSidebar] = useState(false);
 
-  return <SNav />;
+  const showSidebar = () => setSidebar(!sidebar);
+  return (
+    <>
+      <SNav className="navbar">
+        <Link to="#" className="menu-bars">
+          <FontAwesomeIcon icon={faBars} onClick={showSidebar} />
+        </Link>
+      </SNav>
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-items">
+          <li className="navbar-toggle">
+            <Link to="#" className="menu-times">
+              <FontAwesomeIcon icon={faTimes} onClick={showSidebar} />
+            </Link>
+          </li>
+          {SideNavData.map((item, index) => {
+            return (
+              <li key={index} className={item.cName}>
+                <Link to={item.path}>{item.icon}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </>
+  );
 };
 
-export default NavBar;
-
-/*
-Logo
-width: 48px;
-height: auto;
-*/
+export default SideNavBar;
--- AppQuimica/client/src/containers/home/home.jsx
+++ AppQuimica/client/src/containers/home/home.jsx
@@ -2,24 +2,26 @@ import React, { useContext } from "react";
 import Footer from "../../components/common/footer/Footer";
 import NavBar from "../../components/common/navBar/NavBar";
 import styled from "styled-components";
-import { User } from '../../App';
+import { User } from "../../App";
+import SideNavBar from "../../components/common/sideNav/SideNav";
 
 const Heading = styled.h1`
-    text-align: center;
-    margin-top: 30px;
-  `;
+  text-align: center;
+  margin-top: 30px;
+`;
 
 const HomePage = () => {
-
   const { user } = useContext(User);
 
-  const msg = user ? <Heading>Hello {user.username}</Heading> : <Heading>You're not logged in</Heading>;
+  const msg = user ? (
+    <Heading>Hello {user.username}</Heading>
+  ) : (
+    <Heading>You're not logged in</Heading>
+  );
 
   return (
     <div>
-      <NavBar />
-      {msg}
-      <Footer />
+      <SideNavBar />
     </div>
   );
 };
