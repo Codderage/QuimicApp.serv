7997375440f6f5df20766b29178b8fcd60dd0c08
Toni <tonisoria2000@gmail.com>
2021-05-12 20:14:12 +0200

    logout

--- AppQuimica/client/src/components/common/sideNav/SideNav.jsx
+++ AppQuimica/client/src/components/common/sideNav/SideNav.jsx
@@ -2,24 +2,51 @@ import React, { useState, useContext } from "react";
 import { SNav } from "./SideNav.styled";
 import { faBars, faTimes, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import "./SideNav.css";
 import { SideNavData } from "./SideNavData";
 import { User } from "../../../App";
-// import { Logout } from "./Logout";
+import axios from "axios";
+import swal from "sweetalert";
 
 const SideNavBar = () => {
-  const { user } = useContext(User);
-
+  const { user, setUser } = useContext(User);
   const [sidebar, setSidebar] = useState(false);
+  const history = useHistory();
+
+  const logout = async () => {
+    try {
+      const out = await axios.post('logout', {}, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
+      swal({
+        title: "Esperamos volver a verte",
+        text: "  ",
+        icon: "success",
+        button: false,
+        timer: "1800",
+      });
+      setUser(null);
+      localStorage.clear();
+      history.push('/login');
+    } catch (e) {
+      swal({
+        title: "Error interno",
+        text: "Vuelve a intentarlo en unos momentos...",
+        icon: "error",
+        button: false,
+        timer: "3000",
+      });
+      // history.push("/");
+    }
+
+  }
 
   const logged = user ? (
     <span>
-      Welcome {user.username} |{" "}
-      {/* <Link onClick={console.log("HOLA")} className="logged"> */}
-      <Link to="/login" className="logged">
-        Logout
-      </Link>
+      Welcome {user.username} | <Link onClick={logout} className="logged">Logout</Link>
     </span>
   ) : (
     <Link to="/login" className="logged">
@@ -45,14 +72,14 @@ const SideNavBar = () => {
     //   }
     // });
     SideNavData.map((item, index) => {
-      if (user.id_profesor != null) {
+      if (user.id_profesor !== null) {
         return (
           <li key={index} className={item.cName}>
             <Link to={item.path}>{item.icon}</Link>
           </li>
         );
       } else {
-        if (item.path != "/componentes" && item.path != "/compuestos") {
+        if (item.path !== "/componentes" && item.path !== "/compuestos") {
           return (
             <li key={index} className={item.cName}>
               <Link to={item.path}>{item.icon}</Link>
